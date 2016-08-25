fecb78df1b08f9eb00c8f267afdf59296ac255d6
Michael Hartl <michael@michaelhartl.com>
2016-08-25 10:36:12 -0700

    Polish Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  # Returns sanitized HTML from input text using GitHub-flavored Markdown.
+  def markdown(text)
+    sanitize Kramdown::Document.new(text, input: 'GFM').to_html
+  end
 end
